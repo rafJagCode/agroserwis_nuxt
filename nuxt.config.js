3d19af822588407bc32f2a1d27e4f94a62d4135f
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-09-10 23:58:00 +0200

    Changed router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,15 +82,10 @@ export default {
     },
   },
 
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+
   router: {
     base: '/agroserwis/',
-  },
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    publicPath: '/agoroserwis/_nuxt/',
-  },
-  router: {
     scrollBehavior: function (to, from, savedPosition) {
       return { x: 0, y: 0 }
     },
