2c445af9b72fd1c1ccfb5b70e493ebcd94922146
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-03-07 04:15:16 +0100

    Rewrote navbar component

---
+++ components/navbar/AppNavbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app-bar class="navbar" color="primary" height="66px">
+    <div class="content">
+      <Logo></Logo>
+      <Menu @toggle-sidebar="toggleSidebar()"></Menu>
+      <Sidebar
+        @toggle-sidebar="toggleSidebar()"
+        :showSidebar="showSidebar"
+      ></Sidebar>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+import Logo from './Logo'
+import Menu from './Menu'
+import Sidebar from './Sidebar'
+export default {
+  components: {
+    Logo,
+    Menu,
+    Sidebar,
+  },
+  data: () => ({
+    showSidebar: false,
+  }),
+  methods: {
+    toggleSidebar() {
+      this.showSidebar = !this.showSidebar
+    },
+  },
+}
+</script>
+<style scoped lang="scss">
+.navbar {
+  z-index: 2;
+}
+.content {
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+}
+</style>
---
+++ components/navbar/Menu.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="menu">
+    <ul class="menu-list d-flex">
+      <li class="menu-item" v-for="item in items" :key="item.text">
+        <v-btn color="accent" :nuxt="item.nuxt" text :href="item.link">{{
+          item.text
+        }}</v-btn>
+      </li>
+      <li class="hamburger">
+        <v-btn
+          class="accent--text"
+          text
+          elevation="false"
+          @click="$emit('toggle-sidebar')"
+        >
+          <v-icon>mdi-menu</v-icon>
+        </v-btn>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import data from '/static/navbar/data.json'
+export default {
+  data: () => ({
+    items: data,
+  }),
+}
+</script>
+
+<style scoped lang="scss">
+.menu {
+  grid-column: 3/4;
+}
+.menu-list {
+  height: 100%;
+  align-items: center;
+}
+.hamburger {
+  display: none;
+}
+@media (max-width: 960px) {
+  .menu-item {
+    display: none;
+  }
+  .hamburger {
+    display: unset;
+  }
+}
+</style>
