a959b05f22d92c039343b56d7c0bef8c9624db34
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-03-07 04:14:38 +0100

    Rewrote logo component

---
+++ components/navbar/Logo.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-btn class="logo" icon :ripple="false" href="/" height="100%" width="unset">
+    <v-img class="logo-img" :src="`/navbar/images/logo.png`" contain></v-img>
+  </v-btn>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss">
+::v-deep.logo {
+  & .v-btn__content {
+    height: 58px;
+    width: 75px;
+  }
+}
+.logo:before {
+  background: transparent;
+}
+</style>
