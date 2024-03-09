a2c2c305597b076e1b1a86c657e445021ded96e3
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-03-09 01:06:08 +0100

    Added caption to departments section

---
+++ components/home/departments/DepartmentsCaption.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="departments-caption d-flex flex-column">
+    <h4 class="text-h4 accent--text">What We Do?</h4>
+    <h1 class="departments-text text-h1 text-center">
+      We are giving you perfect solutions with our proficient services.
+    </h1>
+    <v-divider class="departments-hr mt-4"></v-divider>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style scoped lang="scss">
+.departments-caption {
+  justify-content: center;
+  align-items: center;
+  row-gap: 1.5rem;
+  margin-bottom: 10rem;
+}
+.departments-text {
+  width: 80rem;
+}
+.departments-hr {
+  width: 50rem;
+  background-color: var(--v-accent-base);
+}
+</style>
