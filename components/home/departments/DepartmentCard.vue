703239fdbcafff74729870b67b2ac9151f19ea4e
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-03-21 02:36:53 +0100

    Improved responsive layout of department cards

--- components/home/departments/DepartmentCard.vue
+++ components/home/departments/DepartmentCard.vue
@@ -5,25 +5,25 @@
       :src="`/departments/images/${department.img}`"
       contain
     ></v-img>
-    <v-card-title class="card-title app-text-h2">{{
-      department.name
-    }}</v-card-title>
-    <v-card-text class="card-text d-flex align-center app-text-info">
-      <p>{{ department.mobile }}</p>
-      <p>{{ department.address }}</p>
-      <p>{{ department.street }}</p>
-    </v-card-text>
-    <v-card-actions>
-      <v-btn
-        class="app-text-button"
-        color="accent"
-        text
-        plain
-        :href="department.link"
-        target="_blank"
-        >Link<v-icon size="12">mdi-arrow-right-thin</v-icon></v-btn
-      ></v-card-actions
-    >
+    <div class="card-text">
+      <p class="card-title app-text-h2">{{ department.name }}</p>
+      <div class="card-info app-text-info">
+        <p>{{ department.mobile }}</p>
+        <p>{{ department.address }}</p>
+        <p>{{ department.street }}</p>
+      </div>
+      <div>
+        <v-btn
+          class="app-text-button"
+          color="accent"
+          text
+          plain
+          :href="department.link"
+          target="_blank"
+          >Link<v-icon size="12">mdi-arrow-right-thin</v-icon></v-btn
+        >
+      </div>
+    </div>
   </v-card>
 </template>
 
@@ -39,25 +39,46 @@ export default {
   height: 100%;
   width: 100%;
   display: grid;
-  align-content: space-between;
+  justify-items: center;
+  align-items: center;
+  gap: 10%;
   box-shadow: 0px 3px 1px -2px rgba(133, 174, 67, 0.2),
     0px 2px 2px 0px rgba(133, 174, 67, 0.14),
     0px 1px 5px 0px rgba(133, 174, 67, 0.12) !important;
-  & * {
-    justify-content: center;
-  }
 }
 .card-img {
-  height: 128px;
+  max-width: 128px;
 }
 .card-text {
-  flex-direction: column;
-  & * {
-    margin: 1rem 2rem;
-    text-align: center;
+  width: fit-content;
+  height: fit-content;
+  display: grid;
+  justify-content: center;
+  text-align: center;
+}
+.card-text p {
+  margin-bottom: 0.5rem;
+}
+@media (width>1150px) and (width<1560px) {
+  .card {
+    grid-auto-flow: column;
+    & .card-img {
+      justify-self: end;
+    }
+    & .card-text {
+      justify-self: start;
+    }
   }
-  & :first-child {
-    flex-basis: 100%;
+}
+@media (width>500px) and (width<800px) {
+  .card {
+    grid-auto-flow: column;
+    & .card-img {
+      justify-self: end;
+    }
+    & .card-text {
+      justify-self: start;
+    }
   }
 }
 </style>
