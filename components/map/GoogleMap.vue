075462298b81b6f59d848abb3c910109c8fb3437
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-03-25 21:54:24 +0100

    Simplified google map component

--- components/map/GoogleMap.vue
+++ components/map/GoogleMap.vue
@@ -1,27 +1,19 @@
 <template>
-  <div class="google-map px-2">
+  <div class="google-map">
     <iframe
       :width="$vuetify.breakpoint.xs ? 280 : 450"
       height="250"
-      frameborder="0" style="border:0"
-      src="https://www.google.com/maps/d/embed?mid=1MMjeLuXoCT3pV35rct2fUn8GbqbOe2w5&hl=pl" allowfullscreen>
+      frameborder="0"
+      style="border: 0"
+      src="https://www.google.com/maps/d/embed?mid=1MMjeLuXoCT3pV35rct2fUn8GbqbOe2w5&hl=pl"
+      allowfullscreen
+    >
     </iframe>
   </div>
 </template>
 
 <script>
-export default {
-}
+export default {}
 </script>
 
-<style scoped>
-.google-map{
-  margin: auto;
-}
-@media (max-width: 1750px){
-  .google-map{
-    margin:auto;
-  }
-}
-
-</style>
+<style scoped></style>
