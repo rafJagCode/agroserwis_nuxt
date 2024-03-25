941ab52d2c8cc564cf34f23ab0af39322206aabc
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-03-25 21:54:02 +0100

    Refactored footer component

---
+++ components/footer/AppFooter.vue
@@ -0,0 +1,55 @@
+<template>
+  <footer class="app-footer">
+    <div class="content">
+      <FooterSection
+        v-for="section in sections"
+        :key="section.title"
+        :section="section"
+      />
+      <GoogleMap />
+    </div>
+    <div class="copyright">
+      <strong>{{ new Date().getFullYear() }} — Agroserwis</strong>
+    </div>
+  </footer>
+</template>
+
+<script>
+import FooterSection from './FooterSection'
+import GoogleMap from '~/components/map/GoogleMap'
+import data from '/static/footer/data.json'
+export default {
+  components: { FooterSection, GoogleMap },
+  data: () => ({
+    sections: data,
+  }),
+}
+</script>
+<style scoped lang="scss">
+.app-footer {
+  padding: 5rem 3rem 0 3rem;
+  display: grid;
+  justify-content: center;
+  background: var(--v-secondary-darken1);
+}
+.content {
+  width: fit-content;
+  display: flex;
+  gap: 8rem;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+.copyright {
+  margin-top: 3rem;
+  font-size: 1.5rem;
+  padding: 0.5rem;
+  width: 100%;
+  text-align: center;
+}
+@media (max-width: 600px) {
+  .content {
+    display: grid;
+    gap: 3rem;
+  }
+}
+</style>
--- components/footer/FooterSection.vue
+++ components/footer/FooterSection.vue
@@ -1,66 +1,73 @@
 <template>
-<div class="footer-section pa-1">
-  <p class="footer-section__name text-h6 ml-4">{{section.name}}</p>
-  <FooterSectionInfo v-if="data.type === 'info'" v-for="data in section.data" :key="data.name" :data="data"></FooterSectionInfo>
-  <FooterSectionContact v-if="data.type === 'contact'" v-for="data in section.data" :key="data.name" :data="data"></FooterSectionContact>
-  <FooterSectionLink v-if="data.type === 'link'" v-for="data in section.data" :key="data.value" :data="data"></FooterSectionLink>
-</div>
+  <div class="section">
+    <h4 class="section-title">{{ section.title }}</h4>
+    <template v-for="item in section.items">
+      <v-btn
+        v-if="item.type === 'link'"
+        color="accent"
+        plain
+        :href="item.href"
+        :key="item.value"
+        class="section-link"
+        >{{ item.value }}</v-btn
+      >
+      <span v-else class="section-text">{{ item.value }}</span>
+    </template>
+  </div>
 </template>
-
 <script>
-import FooterSectionContact from "~/components/footer/FooterSectionContact";
-import FooterSectionLink from "~/components/footer/FooterSectionLink";
-import FooterSectionInfo from "~/components/footer/FooterSectionInfo";
 export default {
-  components: {FooterSectionInfo, FooterSectionLink, FooterSectionContact},
   props: {
-    section: null
+    section: null,
   },
 }
 </script>
 
-<style lang="scss">
-.footer-section{
-  font-family: "Roboto", sans-serif;
-  color: var(--v-primary-base);
-  font-size: 0.875rem;
-  font-weight: 500;
-  white-space: normal;
-  flex-grow: 1;
-  max-width: 500px;
+<style scoped lang="scss">
+.section {
+  display: grid;
+  align-items: center;
+  font-family: Libre Baskerville, serif;
+  height: fit-content;
 }
-.footer-section__name{
-  text-transform: capitalize;
-  position: relative;
-  width: max-content;
-  max-width: 100%;
-  &::before{
-    content: '';
-    position: absolute;
-    top: 0;
-    left:0;
-    height:100%;
-    width: 100%;
-    border-bottom: 2px solid var(--v-primary-base);
+.section-title {
+  text-transform: uppercase;
+  font-weight: 700;
+  color: var(--v-primary-darken2);
+  font-size: 2.2rem;
+  height: 3rem;
+  margin-bottom: 3rem;
+}
+::v-deep.section-link {
+  font-weight: 500;
+  font-size: 1.7rem;
+  text-transform: initial;
+  justify-content: start;
+  padding: 1rem 0 !important;
+  height: fit-content !important;
+  width: fit-content;
+  & .v-btn__content {
+    line-height: revert;
+    letter-spacing: revert;
+    text-indent: rever;
   }
 }
-
-.footer-section .v-btn {
-  text-transform:none !important;
-  font-family: "Roboto", sans-serif;
-  color: var(--v-accent-base);
-  font-size: 0.875rem;
-  padding: 1em;
+.section-text {
   font-weight: 500;
-  letter-spacing: normal;
-  &.v-btn--disabled{
-    color:var(--v-accent-base)!important;
-    & .v-icon{
-      color:var(--v-primary-base)!important;
-    }
+  color: var(--v-primary-lighten4);
+  font-size: 1.7rem;
+  padding: 1rem 0;
+}
+@media (max-width: 600px) {
+  .section {
+    display: grid;
+    justify-items: center;
   }
-  & .v-btn__content{
-    justify-content: left;
+  .section-title {
+    margin-bottom: 1rem;
+  }
+  .section-link {
+    justify-content: center;
   }
 }
 </style>
