5fe1e2197d27828e5827502a8e50506e36660921
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-04-12 02:47:29 +0200

    Added anchor elements to handle navbars scroll padding issue

---
+++ components/anchor/Anchor.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="anchor" :id="id"></div>
+</template>
+<script>
+export default {
+  props: { id: null },
+}
+</script>
+
+<style scoped lang="scss">
+.anchor {
+  visibility: hidden;
+  height: 0;
+  position: absolute;
+  top: calc(var(--navbar-height) * -1);
+}
+</style>
